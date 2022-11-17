d878361acf658d622c32637060c551ea4822f4af
JoasA <34966120+CyberSecurityUP@users.noreply.github.com>
2022-11-17 18:15:52 -0300

    Update PEscan.py

--- Modulo 4/PEscan.py
+++ Modulo 4/PEscan.py
@@ -11,5 +11,9 @@ def func5():
         # abaixo ele printa os hexadeciminal do cabeçalho DOS e_magic e lfanew
 	#print("e_magic : " + hex(pe.DOS_HEADER.e_magic)) # Prints the e_magic field of the DOS_HEADER
 	#print("e_lfnew : " + hex(pe.DOS_HEADER.e_lfanew)) # Prints the e_lfnew field of the DOS_HEADER
+	
+	#e_magic → o número mágico do cabeçalho do DOS é 'MZ' (0x5a4d) e 'MZ' refere-se a Mark Zbikowski , o criador do formato de arquivo executável do MS-DOS.
+#e_lfnew → um ponteiro para o cabeçalho PE (NT Header).
+#Para a maioria dos programas do Windows, o cabeçalho DOS contém um programa DOS que não faz nada além de imprimir “Este programa não pode ser executado no modo DOS” .
 if __name__ == '__main__':
 	func5()
