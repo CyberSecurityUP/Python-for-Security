69576bc27c8146313943ec92d26a1a4edac5c7c9
JoasA <34966120+CyberSecurityUP@users.noreply.github.com>
2022-12-09 21:53:12 -0300

    Update readme.py

--- Scripts Curso/Modulo 1/readme.py
+++ Scripts Curso/Modulo 1/readme.py
@@ -1,15 +1,15 @@
-#with open('readme.txt', 'w') as f:
- #   f.write('test')
+with open('readme.txt', 'w') as f:
+    f.write('test')
 
-#lines = ['Readme', 'Esse é um arquivo criado pelo python', 'dsdsd', 'Matheus', 'ls -lha']
-#with open('readme2.sh', 'w') as f:
- #   for linha in lines:
-  #      f.write(linha)
-   #     f.write('\n')
+lines = ['Readme', 'Esse é um arquivo criado pelo python', 'dsdsd', 'Matheus', 'ls -lha']
+with open('readme2.sh', 'w') as f:
+    for linha in lines:
+        f.write(linha)
+        f.write('\n')
 
-#more_lines = ['add', 'the end']
-#with open('readme2.txt', 'a') as f:
- #   f.write('\n'.join(more_lines))
+more_lines = ['add', 'the end']
+with open('readme2.txt', 'a') as f:
+    f.write('\n'.join(more_lines))
 
 f = open("readme2.txt", "r")
 print(f.read())
